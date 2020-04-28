eb9835236e64a72eecd0ed14ab36e37b93188947
Marcos <marcos.alvarenga@10pines.com>
2020-04-27 22:16:24 -0300

    Ahora un vendedor puede registrar un comercio. Agrega tests a user

--- src/test/java/ar/edu/unq/desapp/comprandoencasa/model/UserTest.java
+++ src/test/java/ar/edu/unq/desapp/comprandoencasa/model/UserTest.java
@@ -1,10 +1,12 @@
 package ar.edu.unq.desapp.comprandoencasa.model;
 
+import ar.com.kfgodel.nary.api.optionals.Optional;
 import ar.edu.unq.desapp.comprandoencasa.extensions.TestUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static ar.edu.unq.desapp.comprandoencasa.model.UserRol.BUYER;
 import static ar.edu.unq.desapp.comprandoencasa.model.UserRol.SELLER;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -29,6 +31,29 @@ public class UserTest {
     @Test
     public void whenWantCreateUserWithMalformedEmail_thenTheUserIsNotCreated() {
         assertInvalidEmail("carlosgmail.com");
+    }
+
+    @Test
+    public void whenWantCreateUserWithSellerRol_thenTheUserIsSeller() {
+        User user = User.createWithoutCommerce("carlos", "gonzalez", "carlos@gmail.com", SELLER);
+
+        assertThat(user.isSeller(), is(true));
+    }
+
+    @Test
+    public void whenWantCreateUserWithBuyerRol_thenTheUserIsNotSeller() {
+        User user = User.createWithoutCommerce("carlos", "gonzalez", "carlos@gmail.com", BUYER);
+
+        assertThat(user.isSeller(), is(false));
+    }
+
+    @Test
+    public void whenWantCreateUserWithCommerce_thenTheUserIsCreated() {
+        Commerce commerce = new Commerce(null, null, null, null, null);
+        User user = User.createWithCommerce("carlos", "gonzalez", "carlos@gmail.com", SELLER, commerce);
+
+        Optional<Commerce> userCommerce = user.getCommerce();
+        assertThat(userCommerce.equals(Optional.ofNullable(commerce)), is(true));
     }
 
     private void assertInvalidEmail(String email) {
