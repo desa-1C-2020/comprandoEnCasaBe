64e6dcc03514cafd0b52d23b57b3e0c955716dc0
Camila Sosa <camila.sosa@neoris.com>
2020-05-03 18:59:34 -0300

    ProductService agregado

--- src/main/java/ar/edu/unq/desapp/comprandoencasa/controllers/ProductController.java
+++ src/main/java/ar/edu/unq/desapp/comprandoencasa/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package ar.edu.unq.desapp.comprandoencasa.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,36 +9,48 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import ar.edu.unq.desapp.comprandoencasa.model.Product;
+import ar.edu.unq.desapp.comprandoencasa.service.ProductService;
+
 @RestController
 @RequestMapping(value = ProductController.basePath)
 public class ProductController {
 
     public static final String basePath = "/products";
 
+    @Autowired
+    private ProductService productService;
+    
     @PostMapping("/saveProduct")
     @ResponseStatus(value = HttpStatus.OK)
-    public void saveNewProduct(@RequestParam("shop") String shopID,
+    public void saveNewProduct(@RequestParam("shopID") String shopID,
     						   @RequestParam("name") String name,
-                               @RequestParam("brand") String marca,
+                               @RequestParam("brand") String brand,
                                @RequestParam("stock") String stock,
                                @RequestParam("price") String price,
                                @RequestParam("imageUrl") String imageUrl) {
-    	
+    	int productStock = Integer.parseInt(stock);
+    	double productPrice = Integer.parseInt(price);
+    	Product product = new Product(name, brand, productStock, productPrice, imageUrl);
+    	int id = Integer.parseInt(shopID);
+    	this.productService.saveProduct(id, product);
     } 
     
     @DeleteMapping("/deleteProduct")
     @ResponseStatus(value = HttpStatus.OK)
-    public void deleteExistingProduct(@RequestParam("shop") String shopID,
-    									  @RequestParam("productID") String productID) {
-    	
+    public void deleteExistingProduct(@RequestParam("shopID") String shopID,
+    								  @RequestParam("productID") String productID) {
+    	int idShop = Integer.parseInt(shopID);
+    	int idProduct = Integer.parseInt(productID);
+    	this.productService.deleteProduct(idShop, idProduct);
     } 
-    
     
     @PostMapping("/modifyProduct")
     @ResponseStatus(value = HttpStatus.OK)
-    public void modifyExistingProduct(@RequestParam("shop") String shopID,
-    									  @RequestParam("productID") String productID) {
-    	
+    public void modifyExistingProduct(@RequestParam("shopID") String shopID,
+    								  @RequestParam("product") Product product) {
+    	int idShop = Integer.parseInt(shopID);
+    	this.productService.modifyProduct(idShop, product);
     } 
   
 }
--- src/main/resources/ar/edu/unq/desapp/comprandoencasa/controller/ProductControllerTest.java
+++
@@ -1,5 +0,0 @@
-package ar.edu.unq.desapp.comprandoencasa.controller;
-
-public class ProductControllerTest {
-
-}
