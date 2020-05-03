e443061cdfa8f512118b36119a3f3e015bbb62fc
Marcos <marcos.alvarenga@10pines.com>
2020-05-03 06:59:16 -0300

    Se agregan cosas necesarias para jackson. Ver si se puede cambiar por alguna froma mas limpia de mapear los objetos

--- src/main/java/ar/edu/unq/desapp/comprandoencasa/model/Commerce.java
+++ src/main/java/ar/edu/unq/desapp/comprandoencasa/model/Commerce.java
@@ -1,13 +1,41 @@
 package ar.edu.unq.desapp.comprandoencasa.model;
 
+import com.google.maps.model.LatLng;
+
 import java.util.List;
 
 public class Commerce {
-    private final String businessSector;
-    private final String adress;
-    private final List<String> paymentMethods;
-    private final List<String> daysAndHoursOpen;
-    private final String range;
+    private String businessSector;
+    private String adress;
+    private List<String> paymentMethods;
+    private List<String> daysAndHoursOpen;
+
+    public void setBusinessSector(String businessSector) {
+        this.businessSector = businessSector;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public void setPaymentMethods(List<String> paymentMethods) {
+        this.paymentMethods = paymentMethods;
+    }
+
+    public void setDaysAndHoursOpen(List<String> daysAndHoursOpen) {
+        this.daysAndHoursOpen = daysAndHoursOpen;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    private String range;
+    private LatLng latLng;
+
+    public Commerce(){
+
+    }
 
     public Commerce(String businessSector, String adress, List<String> paymentMethods, List<String> daysAndHoursOpen,
                     String range) {
@@ -16,5 +44,33 @@ public class Commerce {
         this.paymentMethods = paymentMethods;
         this.daysAndHoursOpen = daysAndHoursOpen;
         this.range = range;
+    }
+
+    public String getBusinessSector() {
+        return businessSector;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public List<String> getPaymentMethods() {
+        return paymentMethods;
+    }
+
+    public List<String> getDaysAndHoursOpen() {
+        return daysAndHoursOpen;
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public LatLng getLatLong() {
+        return latLng;
+    }
+
+    public void setLatLng(LatLng latLng){
+        this.latLng = latLng;
     }
 }
