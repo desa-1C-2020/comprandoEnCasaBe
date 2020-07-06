c6412854ff6abfc0b117a2281034a38494812317
Marcos <marcos.alvarenga@10pines.com>
2020-07-05 23:42:04 -0300

    Configure security. Required Oatuh authentication in requests

--- src/main/java/ar/edu/unq/desapp/comprandoencasa/ComprandoEnCasaApplication.java
+++ src/main/java/ar/edu/unq/desapp/comprandoencasa/ComprandoEnCasaApplication.java
@@ -2,11 +2,13 @@ package ar.edu.unq.desapp.comprandoencasa;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 /**
  * Spring App entrypoint
  */
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ComprandoEnCasaApplication {
 
     public static void main(String[] args) {
