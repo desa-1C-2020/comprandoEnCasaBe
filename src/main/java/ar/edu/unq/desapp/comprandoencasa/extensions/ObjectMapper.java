b2035127239b8bacf988db46645bbdba10db9328
Marcos <marcos.alvarenga@10pines.com>
2020-07-12 18:32:33 -0300

    Fix shopping list

--- src/main/java/ar/edu/unq/desapp/comprandoencasa/extensions/ObjectMapper.java
+++ src/main/java/ar/edu/unq/desapp/comprandoencasa/extensions/ObjectMapper.java
@@ -25,7 +25,6 @@ public class ObjectMapper {
         ShoppingListTo shoppingListTo = new ShoppingListTo();
         shoppingListTo.setTotal(shoppingList.getTotal());
         shoppingListTo.setCreationDateTime(shoppingList.getCreationDateTime());
-        shoppingListTo.setUserId(shoppingList.getUserId());
         shoppingListTo.setItemByCommerceTo(itemByCommerceTos);
         return null;
     }
