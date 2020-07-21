18cdc264ad6f18a67f2383dfe5ab908038d34934
Marcos <marcos.alvarenga@10pines.com>
2020-07-21 19:34:36 -0300

    USe junit 4 to run tests

--- src/test/java/ar/edu/unq/desapp/architecture/InterfacesRulesTest.java
+++ src/test/java/ar/edu/unq/desapp/architecture/InterfacesRulesTest.java
@@ -1,24 +1,34 @@
 package ar.edu.unq.desapp.architecture;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchUnitRunner;
+import org.junit.runner.RunWith;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.GeneralCodingRules.ACCESS_STANDARD_STREAMS;
 
-import org.junit.Test;
-
+@RunWith(ArchUnitRunner.class)
+@AnalyzeClasses(packages = "main.java.ar.edu.unq.desapp.comprandoencasa")
 public class InterfacesRulesTest {
-
-	private final JavaClasses classes = new ClassFileImporter().importPackages("ar.edu.unq.desapp.comprandoencasa.controller");
-
-    @Test
-    public void interfaces_names_should_not_contain_the_word_interface() {
-    	noClasses().that().areInterfaces().should().haveSimpleNameContaining("Interface").check(classes);
-    }
-    
-    @Test
-    public void interfaces_should_not_be_contained_in_impl_packages() {
-    	noClasses().that().areInterfaces().should().haveSimpleNameContaining("Interface").check(classes);
+    @ArchTest
+    public void interfaces_should_not_have_simple_class_names_containing_the_word_interface(JavaClasses classes) {
+        noClasses().that().areInterfaces().should().haveSimpleNameContaining("Interface").check(classes);
     }
 
+    @ArchTest
+    public void interfaces_must_not_be_placed_in_implementation_packages(JavaClasses classes) {
+        noClasses().that().resideInAPackage("..impl..").should().beInterfaces().check(classes);
+    }
+
+    @ArchTest
+    public void no_access_to_standard_streams_as_method(JavaClasses classes) {
+        noClasses().should(ACCESS_STANDARD_STREAMS).check(classes);
+    }
+
+    @ArchTest
+    public void interfaces_should_not_have_names_ending_with_the_word_interface(JavaClasses classes) {
+        noClasses().that().areInterfaces().should().haveNameMatching(".*Interface").check(classes);
+    }
 }
--- src/test/java/ar/edu/unq/desapp/architecture/NamingConventionTest.java
+++ src/test/java/ar/edu/unq/desapp/architecture/NamingConventionTest.java
@@ -1,70 +1,73 @@
 package ar.edu.unq.desapp.architecture;
 
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchUnitRunner;
+import org.junit.runner.RunWith;
+
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
-import org.junit.Test;
 
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
+@RunWith(ArchUnitRunner.class)
+@AnalyzeClasses(packages = "main.java.ar.edu.unq.desapp")
 public class NamingConventionTest {
-	
-	
-	private final JavaClasses classes = new ClassFileImporter().importPackages("ar.edu.unq.desapp");
-	
-    @Test
-    public void transfer_objects_should_be_subfixed_with_to() {
-    	classes()
-        .that().resideInAPackage("..to")
-        .should().haveSimpleNameEndingWith("TO")
-        .check(classes);
+
+    @ArchTest
+    public void transfer_objects_should_be_subfixed_with_to(JavaClasses classes) {
+        classes()
+            .that().resideInAPackage("..to")
+            .should().haveSimpleNameEndingWith("TO")
+            .check(classes);
     }
-    
-    @Test
-    public void controllers_should_be_subfixed_with_controller() {
-    	classes()
-        .that().resideInAPackage("..controllers")
-        .should().haveSimpleNameEndingWith("Controller")
-        .check(classes);	
+
+    @ArchTest
+    public void controllers_should_be_subfixed_with_controller(JavaClasses classes) {
+        classes()
+            .that().resideInAPackage("..controllers")
+            .should().haveSimpleNameEndingWith("Controller")
+            .check(classes);
     }
-	@Test
-    public void services_should_be_subfixed_with_service() {
-    	classes()
-        .that().resideInAPackage("..service")
-        .and().areInnerClasses()
-        .should().haveSimpleNameEndingWith("Service")
-        .check(classes);
+
+    @ArchTest
+    public void services_should_be_subfixed_with_service(JavaClasses classes) {
+        classes()
+            .that().resideInAPackage("..service")
+            .and().areInnerClasses()
+            .should().haveSimpleNameEndingWith("Service")
+            .check(classes);
     }
-    
-    @Test
-    public void aspects_should_be_subfixed_with_aspect() {
-    	classes()
-        .that().resideInAPackage("..aspects")
-        .should().haveSimpleNameEndingWith("Aspect")
-        .check(classes);
+
+    @ArchTest
+    public void aspects_should_be_subfixed_with_aspect(JavaClasses classes) {
+        classes()
+            .that().resideInAPackage("..aspects")
+            .should().haveSimpleNameEndingWith("Aspect")
+            .check(classes);
     }
-    
-    @Test
-    public void exceptions_should_be_subfixed_with_exception() {
-    	classes()
-        .that().resideInAPackage("..exception")
-        .should().haveSimpleNameEndingWith("Exception")
-        .check(classes);
+
+    @ArchTest
+    public void exceptions_should_be_subfixed_with_exception(JavaClasses classes) {
+        classes()
+            .that().resideInAPackage("..exception")
+            .should().haveSimpleNameEndingWith("Exception")
+            .check(classes);
     }
-    
-    @Test
-    public void repositories_should_be_subfixed_with_repository() {
-    	classes()
-        .that().resideInAPackage("..repositories")
-        .should().haveSimpleNameEndingWith("Repository")
-        .check(classes);
+
+    @ArchTest
+    public void repositories_should_be_subfixed_with_repository(JavaClasses classes) {
+        classes()
+            .that().resideInAPackage("..repositories")
+            .should().haveSimpleNameEndingWith("Repository")
+            .check(classes);
     }
-    
-    @Test
-    public void implementations_should_be_subfixed_with_impl() {
-    	classes()
-        .that().resideInAPackage("..impl")
-        .should().haveSimpleNameEndingWith("Impl")
-        .check(classes);
+
+    @ArchTest
+    public void implementations_should_be_subfixed_with_impl(JavaClasses classes) {
+        classes()
+            .that().resideInAPackage("..impl")
+            .should().haveSimpleNameEndingWith("Impl")
+            .check(classes);
     }
-    
+
 }
